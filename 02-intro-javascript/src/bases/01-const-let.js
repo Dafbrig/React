570abd88b79f73e4e215c6d8d1c6a2f4cd953d80
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-03-19 19:10:33 -0500

    Inicio de variables
    Let and Const

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y Constantes
+//const no se renombran, no cambia valores
+// let se puede renombrar, cambia los valores
+const nombre = 'Taco';
+const apellido = 'de carne';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+//var no se debe usar...
+
+if (true){
+    let valorDado = 6;
+    console.log(valorDado);
+}
