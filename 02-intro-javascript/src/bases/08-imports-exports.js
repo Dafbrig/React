69a98162e1af7d52a5f9a763391be33d6a20106e
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-03-24 14:57:53 -0500

    08
    imporst

---
+++ 02-intro-javascript/src/bases/08-imports-exports.js
@@ -0,0 +1,24 @@
+import {heroes} from './data/heroes';
+
+//const getHeroesById = (id) => {
+    // eslint-disable-next-line array-callback-return
+    //return heroes.find( (heroe) => {
+       // if(heroe.id === id){
+          //  return true;
+        //}else{
+      //      return false;
+    //    }
+  //  });
+//}
+
+const getHeroesById = (id) => {
+    // eslint-disable-next-line array-callback-return, no-unused-expressions
+    return heroes.find( (heroe) =>  heroe.id === id );
+}
+
+console.log(getHeroesById(2));
+
+//find?, filter 
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
+
+console.log(getHeroesByOwner('DC'));
