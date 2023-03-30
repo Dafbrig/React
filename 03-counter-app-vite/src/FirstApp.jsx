ffdc2ff50825338915f285fa40b9685efab58500
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-03-30 15:25:33 -0500

    Componente
    Componentes

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,13 @@
+//export function App(){
+//    // document.createElement...
+//    return (<h1>Hola Mundo</h1>)
+//}
+
+import React from 'react'
+
+export const FirstApp = () => {
+  return (
+    <h1>FirstApp</h1>
+  );
+}
+
