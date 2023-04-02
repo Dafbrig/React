1bcc1c19ad7b5dce0224192acf75711aae7b55b8
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-04-02 15:23:41 -0500

    Seccion 4

--- 03-counter-app-vite/src/SecondApp.jsx
+++ 03-counter-app-vite/src/SecondApp.jsx
@@ -1,9 +1,28 @@
-export const SecondApp = () => {
+import { PropTypes } from "prop-types";
+
+export const SecondApp = ( tittle, subTittle, name ) => {
+
+
+
+  //console.log( props );
+
   return (
     <>
-        <h1>David</h1>
-        <p>Soy un subtitulo</p>
+        <h1>{ tittle }</h1>
+        {/*<code>{JSON.stringify(newMessage)}</code>*/}
+        <p>{ subTittle }</p>
+        <p> { name }</p>
     </>
   )
 }
 
+SecondApp.propTypes = {
+  tittle: PropTypes.string.isRequired,
+  subTittle: PropTypes.string,
+}
+
+SecondApp.defaultProps = {
+  name: 'David Briceño',
+  subTittle: 'No hay SubTitulo',
+  tittle: 'No hay titulo',
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ReactDOM }  from 'react-dom/client';
-import { SecondApp } from './SecondApp';
+//import { SecondApp } from './SecondApp';
 //import { FirstApp } from './FirstApp';
+import './styles.css';
+import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <SecondApp/>
+        <CounterApp value={10}/>
     </React.StrictMode>
 );
 
